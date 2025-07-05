f774794a34c7b8058f7f16e501d939004751603a
ExistentialismArt <104840099+TheChuan1503@users.noreply.github.com>
2025-07-05 19:19:23 +0800

    init

---
+++ script.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         移除bilibili直播间的马赛克
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-05
+// @description  移除bilibili直播间的马赛克
+// @author       TheChuan1503
+// @match        https://live.bilibili.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function action(){
+        var doms = document.getElementsByClassName('web-player-module-area-mask')
+        for (let i = 0; i < doms.length; i++) {
+            var dom = doms[i]
+            dom.style = {}
+        }
+    }
+    setInterval(() => {
+        action()
+    }, 200)
+})();
