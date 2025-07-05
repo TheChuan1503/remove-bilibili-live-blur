3db665a5e99fcbe934685cc4a030d27ed8f3fb2e
ExistentialismArt <104840099+TheChuan1503@users.noreply.github.com>
2025-07-05 19:22:23 +0800

    edit description

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name         移除bilibili直播间的马赛克
 // @namespace    http://tampermonkey.net/
 // @version      2025-07-05
-// @description  移除bilibili直播间的马赛克
+// @description  自动移除bilibili直播间网页版左下角和右下角的马赛克
 // @author       TheChuan1503
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
