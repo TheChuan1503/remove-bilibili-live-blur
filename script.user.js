08ecdf4a87570234d80ec3d4b3083b3ba1e33554
ExistentialismArt <104840099+TheChuan1503@users.noreply.github.com>
2025-07-05 19:23:23 +0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -3,6 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      2025-07-05
 // @description  自动移除bilibili直播间网页版左下角和右下角的马赛克
+// @description:en Automatically remove the mosaic in the bottom left and right corners of the Bilibili live streaming webpage version
 // @author       TheChuan1503
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
